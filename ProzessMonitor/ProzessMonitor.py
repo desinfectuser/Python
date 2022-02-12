ef52920b8ba50454e909070518a6e35e026ab82a
desinfectuser <95765132+desinfectuser@users.noreply.github.com>
2022-02-12 10:47:46 +0100

    Update ProzessMonitor.py

--- ProzessMonitor/ProzessMonitor.py
+++ ProzessMonitor/ProzessMonitor.py
@@ -1,1 +1,67 @@
+#!/usr/bin/env python3
 
+
+import datetime
+import psutil
+from tabulate import tabulate
+import os
+import time
+
+def get_processes():
+    procs = []
+    for p in psutil.process_iter():
+        with p.oneshot():
+            pid = p.pid
+            if pid == 0:
+                continue
+            name = p.name()
+            try:
+                create_time = datetime.datetime.fromtimestamp(p.create_time())
+            except OSError:
+                create_time = datetime.datetime.fromtimestamp(psutil.boot_time())
+            cpu_usage = p.cpu_percent()
+            try:
+                cpu_affinity = len(p.cpu_affinity())
+            except psutil.AccessDenied:
+                cpu_affinity = "Berechtigung Verweigert"
+            status = p.status()
+            try:
+                memory = p.memory_full_info().uss
+            except psutil.AccessDenied:
+                memory = "Berechtigung verweigert"
+            try:
+                user = p.username()
+            except psutil.AccessDenied:
+                user = "Berechtigung verweigert"
+            nice = p.nice
+        procs.append({
+            'PID': pid,
+            'Name': name,
+            'Erstellungszeit': create_time,
+            'CPU Auslastung': cpu_usage,
+            'Erlaubte CPU-Kerne': cpu_affinity,
+            'Status': status,
+            'Arbeitsspeicher Auslastung': get_size(memory),
+            'Benutzer': user
+            'Wichtigkeit': nice
+        })
+    return procs
+
+def get_size(bytes):
+    for i in ['', 'K', 'M', 'G', 'T', 'P', 'E']:
+        if bytes < 1024:
+            return f"{bytes:.2f}{i}B"
+        bytes /= 1024
+
+def print_processes(ps):
+    print(tabulate(ps, headers="keys", tablefmt='github'))
+
+procs = get_processes()
+while True:
+    print_processes(procs)
+    time.sleep(5)
+    procs = get_processes()
+    if "nt" in os.name:
+        os.system("cls")
+    else:
+        os.system("clear")
